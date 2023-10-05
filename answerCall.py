c50a2be851df60451e7a56b86d4e017620d50777
Gadbois_Collin <gadbc079@hsd.k12.or.us>
2023-10-05 09:09:25 -0700

    progress

--- answerCall.py
+++ answerCall.py
@@ -1,10 +1,30 @@
 # answerCall.py
-# by _______
+# by Collin Gadbois
 
 # For instructions on what to do, see README.md
 # or visit (https://github.com/HundredVisionsGuy/answerCall)
 
 # Write function defintion: answerCall()
+def answerCall():
+    caller_codes = "U","T","R","F"
+    sameAreaCode = True,False
+    cur_time = "09:00","14:00","23:50","10:40","23:00","10:00","13:00","16:00", "04:00", "9:00"
+    if():
+        cur_time = "04:00", "23:50", "23:00"
+    else:
+        return False
+    if():
+        cur_time = "09:00", "14:00", "10:40", "10:00", "13:00", "9:00", "16:00"
+    else:
+        return True
+    if():
+        caller_codes = "T"
+    else:
+        return False
+    if():
+        caller_codes = "U", "R", "F"
+    else:
+        return True
 
 # Make sure it returns a value
 
